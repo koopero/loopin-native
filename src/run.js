f5e35064d2729b8a4c3fb8fb0ce9ad6480304a2b
koopero <koopero@gmail.com>
2016-07-03 18:37:11 -0700

    Cheat to run x-windows

--- src/run.js
+++ src/run.js
@@ -6,8 +6,13 @@ function run( opt ) {
   const build = this
       , execPath = build.resolve( build.app, build.substitute( build.settings['app']['executable'] ) )
       , spawn = require('child_process').spawn
+      , env = _.clone( process.env )
+
+  // Try to connect to main screen on linux systems
+  if ( os.platform() == 'linux' && !env['DISPLAY'] )
+    env['DISPLAY'] = ':0'
 
   build.log( execPath )
-  const process = spawn( execPath, [], { cwd: opt.runCwd } )
+  const process = spawn( execPath, [], { cwd: opt.runCwd, env: env } )
   build.process = process
 }
