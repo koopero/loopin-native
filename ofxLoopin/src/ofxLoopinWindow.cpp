1abb085bd2e0247641fdfd1cee850c3211c3067e
Samm Cooper <koopero@gmail.com>
2017-11-13 17:50:52 -0800

    Detect ES in info

--- ofxLoopin/src/ofxLoopinWindow.cpp
+++ ofxLoopin/src/ofxLoopinWindow.cpp
@@ -6,6 +6,13 @@ Json::Value ofxLoopinWindow::infoGet() {
   shared_ptr<ofBaseGLRenderer> renderer = dynamic_pointer_cast<ofBaseGLRenderer>( ofGetCurrentRenderer() );
 
   if ( renderer ) {
+    bool es = false;
+
+    #ifdef TARGET_OPENGLES
+      es = true;
+    #endif
+
+    result["gl"]["ES"] = es;
     result["gl"]["VersionMajor"] = renderer->getGLVersionMajor();
     result["gl"]["VersionMinor"] = renderer->getGLVersionMinor();
   }
