0440fc9b4c329833a44346cba9122c20141e7cfe
Samm Zuest Cooper <samm@hfour.ca>
2018-05-22 01:28:24 -0700

    first try of syphon

--- ofxLoopin/src/ofxLoopinApp.h
+++ ofxLoopin/src/ofxLoopinApp.h
@@ -22,6 +22,7 @@
 #include "ofxLoopinRender.h"
 #include "ofxLoopinSaver.h"
 #include "ofxLoopinShaders.h"
+#include "ofxLoopinSyphon.h"
 #include "show/show.hpp"
 #include "ofxLoopinText.h"
 #include "ofxLoopinVideo.h"
@@ -139,6 +140,11 @@ public:
     type: osd
   */
   ofxLoopinOSD osd;
+  
+  #ifdef LOOPIN_SYPHON
+  ofxLoopinSyphonRoot syphon;
+  #endif
+
 
   ofxLoopinInfo info;
   // openFrameWorks master overrides.
@@ -192,6 +198,10 @@ protected:
     addSubControl( "waveform", &waveforms );
     // addSubControl( "fft", &fft );
 
+    #ifdef LOOPIN_SYPHON
+    addSubControl( "syphon", &syphon );
+    #endif
+
     addSubControl( "save", &savers );
     addSubControl( "show", &show );
 
@@ -210,6 +220,10 @@ protected:
     renderLists.push_back( &renders );
     renderLists.push_back( &savers );
     renderLists.push_back( &pixels );
+
+    #ifdef LOOPIN_SYPHON
+    renderLists.push_back( &syphon );
+    #endif
   }
 
   // Utility for reading from stdio
