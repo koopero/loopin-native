188688d4e7116f1438ac5fbbf6b2d71201de455d
Samm Zuest Cooper <samm@hfour.ca>
2018-09-30 01:55:09 -0700

    Get zipping working again

--- src/build/zip.js
+++ src/build/zip.js
@@ -1,20 +1,26 @@
 module.exports = zip
-const Promise = require('bluebird')
-    , fs = require('fs-extra')
 
-function zip( build ) {
+const Promise = require('bluebird')
+const fs = require('fs-extra')
+const tar = require('tar')
+const glob = require('glob-promise')
+
+async function zip( build ) {
   build.templatize('binary.package')
 
-  let filelist = [ 'data', build.binary.package ]
 
-  return require('./executable')( build )
-  .then( () => fs.mkdirs( build.resolve( build.upload.dir ) )
-  .then( () => require('tar').c( {
+  await require('./executable')( build )
+
+  let cwd = build.resolve( build.project.root, 'bin' )
+  let filelist = await glob( '*', { cwd } )
+
+  await fs.mkdirs( build.resolve( build.upload.dir ) )
+  
+  await tar.c( {
     file: build.resolve( build.upload.dir, build.project.zipName ),
-    cwd: build.resolve( build.project.root, 'bin' ),
+    cwd,
     gzip: true,
     follow: true,
     portable: false,
-  }, filelist ) )
-
+  }, filelist )
 }
