397a4a12a9259ff378e6e7f08861b522d52691a2
Samm Zuest Cooper <kooper@gmail.com>
2023-04-14 18:25:48 -0700

    Update deps

--- src/patch.js
+++ src/patch.js
@@ -10,40 +10,41 @@ async function main( build, {
   patches,
   dir,
 } ) {
-  await Promise.mapSeries( patches, async ( name ) => {
+  if ( patches && patches.length )
+    await Promise.mapSeries( patches, async ( name ) => {
 
-    let file = pathlib.resolve( __dirname, '..', 'patch', name )
-    let patch = await fs.readFile( file, 'utf8' )
+      let file = pathlib.resolve( __dirname, '..', 'patch', name )
+      let patch = await fs.readFile( file, 'utf8' )
 
-    build.log( `# applying patch ${name}`)
+      build.log( `# applying patch ${name}`)
 
-    await Promise.fromCallback( cb => {
-      Diff.applyPatches( patch, { loadFile, patched, complete } )
+      await Promise.fromCallback( cb => {
+        Diff.applyPatches( patch, { loadFile, patched, complete } )
 
-      function fileForIndex( index ) {
-        let { oldFileName } = index
-        let file = build.resolve( dir, oldFileName )
-        return file
-      }
+        function fileForIndex( index ) {
+          let { oldFileName } = index
+          let file = build.resolve( dir, oldFileName )
+          return file
+        }
 
-      async function loadFile( index, callback ) {
-        console.log( 'loadFile', { index })
-        let file = fileForIndex( index )
-        let data = await fs.readFile( file, 'utf8' )
-        callback( null, data )        
-      }
+        async function loadFile( index, callback ) {
+          console.log( 'loadFile', { index })
+          let file = fileForIndex( index )
+          let data = await fs.readFile( file, 'utf8' )
+          callback( null, data )        
+        }
 
-      async function patched( index, content, callback ) {
-        console.log( 'patched', { index, content })
-        let file = fileForIndex( index )
-        await fs.writeFile( file, content )
-        callback()
-      }
+        async function patched( index, content, callback ) {
+          console.log( 'patched', { index, content })
+          let file = fileForIndex( index )
+          await fs.writeFile( file, content )
+          callback()
+        }
 
-      function complete( err ) {
-        cb( err )
-      }
-    } )
-  })
+        function complete( err ) {
+          cb( err )
+        }
+      } )
+    })
   
 }
